fee2ccdab31f7cc42b66b0ab4c5ed546e04f9474
pratiksha pawar <you@example.com>
2026-06-17 13:26:50 +0530

    Fix Render database connection

--- database.py
+++ database.py
@@ -1,18 +1,34 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not found")
+
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 
 Base = declarative_base()
